47b322754abc9fd1171cab842acdba52c0123138
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-02-20 09:30:27 -0800

    [initialize] express server

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000 || process.env.PORT
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.listen(port, function () {
+    console.log('Example app listening on port 3000!');
+});
