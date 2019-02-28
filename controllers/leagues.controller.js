b2763f36c6e884336f19964ef686b629d497eb65
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-02-28 12:27:21 -0800

    [created] MVC [installed] body-parser

---
+++ controllers/leagues.controller.js
@@ -0,0 +1,7 @@
+const League = require('../models/league.model.js');
+
+module.exports = (app) => {
+    app.post("/leagues", (req, res) => {
+        
+    })
+}
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+
 
 app.get('/', function (req, res) {
     res.send('Soccer API');
