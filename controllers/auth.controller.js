801dae009d4e04d9bcad1804010a5c0c89e45030
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-03-02 18:26:08 -0800

    [encrypted] user info

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,18 @@
-module.exports = (app) => {
-    // SIGN UP FORM
-    app.get("/sign-up", (req, res) => {
-        res.render("sign-up");
+const User = require("../models/user.model");
+
+module.exports = app => {
+    // SIGN UP POST
+    app.post("/sign-up", (req, res) => {
+        // Create User
+        const user = new User(req.body);
+
+        user
+            .save()
+            .then(user => {
+                res.redirect("/");
+            })
+            .catch(err => {
+                console.log(err.message);
+            });
     });
-}
+};
