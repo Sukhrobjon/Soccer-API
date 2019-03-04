0949fcc1ba441bdfbd8b7be7ac69e8c55da85fe7
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-03-03 18:50:41 -0800

    [implemented] login and logout

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -22,4 +22,49 @@ module.exports = app => {
                 return res.status(400).send({ err: err });
         });
     });
+
+    // LOGOUT
+    app.get('/logout', (req, res) => {
+        console.log("logout")
+        res.clearCookie('nToken');
+        res.redirect('/');
+    });
+
+    // LOGIN FORM
+    app.get('/login', (req, res) => {
+        res.render('login');
+    });
+
+
+    // LOGIN
+    app.post("/login", (req, res) => {
+        const username = req.body.username;
+        const password = req.body.password;
+        // Find this user name
+        User.findOne({ username }, "username password")
+            .then(user => {
+                if (!user) {
+                    // User not found
+                    return res.status(401).send({ message: "Wrong Username or Password" });
+                }
+                // Check the password
+                user.comparePassword(password, (err, isMatch) => {
+                    if (!isMatch) {
+                    // Password does not match
+                    return res.status(401).send({ message: "Wrong Username or password" });
+                    }
+                    // Create a token
+                    const token = jwt.sign({ _id: user._id, username: user.username }, process.env.SECRET, {
+                    expiresIn: "60 days"
+                    });
+                    // Set a cookie and redirect to root
+                    res.cookie("nToken", token, { maxAge: 900000, httpOnly: true });
+                    res.redirect("/");
+                });
+            })
+        .catch(err => {
+            console.log(err);
+        });
+    });
+
 };
